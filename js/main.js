00866a1d2e7b1a966cb78c989b5fe3feb0820247
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-04-12 11:49:34 -0400

    update the view on every input

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 class Controller {
     constructor (game) {
         this.game = game;
-        this.input = new Input(command => game.commandHandler(command));
         this.renderer = new HTMLView();
+        this.input = new Input(command => { 
+            let message = game.commandHandler(command)
+            // Update the view on every input
+            this.update()
+            return message;
+        });
 
         this.renderer.render();
         this.renderer.renderBoard(0, this.game.state.players[0]);
         this.renderer.renderBoard(1, this.game.state.players[1]);
-        this.renderer.renderShips(this.game.state.players[0].ships, this.game.state.players[0].board, 0);
-        this.renderer.renderShips(this.game.state.players[1].ships, this.game.state.players[1].board, 1);
+    }
+
+    update () {
+        this.renderer.renderShips(this.game.state.players[0].ships, 
+                                  this.game.state.players[0].board, 0);
+        this.renderer.renderShips(this.game.state.players[1].ships,
+                                  this.game.state.players[1].board, 1);
     }
 }
 
