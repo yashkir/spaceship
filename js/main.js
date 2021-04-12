421e36fd304aebdc2f0a88a17a2fca2eb035ca9f
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-04-12 15:45:39 -0400

    SPLIT Controller from main.js to its own file

--- js/main.js
+++ js/main.js
@@ -1,65 +1,13 @@
-class Controller {
-    constructor (game) {
-        this.game = game;
-        this.renderer = new HTMLView();
-        this.input = new Input(command => { 
-            let message = game.commandHandler(command)
-            // Update the view on every input
-            this.update()
-            return message;
-        });
-
-        let handler = (...args) => {
-            this.clickHandler(...args)
-        };
-
-        [0, 1].forEach((n) => {
-            this.renderer.createBoard(n, this.game.state.players[n].board.width,
-                                         this.game.state.players[n].board.height, handler);
-        });
-
-        this.renderer.updateStatus("Place your ship...");
-    }
-
-    clickHandler(playerId, x, y) {
-        let shipId = 2;
-        console.log(this.game.commandHandler(`place ${playerId} ${shipId} ${x} ${y}`));
-        this.update();
-    }
-
-    update () {
-        for (let n of [0, 1]) {
-            this.renderShips(this.game.state.players[n].ships, n);
-            this.renderHits(this.game.state.players[n].board.hits, n);
-        }
-    }
-
-    renderShips(ships, playerNum) {
-        ships.forEach(ship => {
-            for (let part of ship.parts) {
-                let x = ship.position[0] + part[0];
-                let y = ship.position[1] + part[1];
-                this.renderer.tagSquare(playerNum, x, y, "ship");
-            }
-        });
-    }
-
-    renderHits(hits, boardId) {
-        for (let hit of hits) {
-            let x = hit[0];
-            let y = hit[1];
-            this.renderer.tagSquare(boardId, x, y, "hit")
-        }
-    }
-}
-
-
 let game = new SpaceShipGame();
 let controller = new Controller(game);
 
 // Test setup
 {
     game.placeShip(0, 1, 2, 2);
+    game.placeShip(0, 1, 5, 2);
     game.placeShip(1, 1, 3, 2);
+    game.selectSquare(0, 2, 2);
+    game.resolveFire(0);
+    //game.clearSelection();
     controller.update();
 }
