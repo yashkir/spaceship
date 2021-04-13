fdb9c596fd50981e55bcbc09ba128c1d524a014a
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-04-13 12:22:26 -0400

    add a test setup

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
 let game = new SpaceShipGame();
-let controller = new Controller(game);
-
 // Test setup
 {
-    game.placeShip(0, 1, 2, 2);
-    game.placeShip(0, 1, 5, 2);
     game.placeShip(1, 1, 3, 2);
-    game.selectSquare(0, 2, 2);
-    game.resolveFire(0);
-    //game.clearSelection();
-    controller.update();
+    game.placeShip(1, 2, 5, 5);
+    game.placeShip(1, 3, 7, 7);
 }
+let controller = new Controller(game);
